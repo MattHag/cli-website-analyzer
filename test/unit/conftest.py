e096f79416d50bc350aa075c028a20d848eb1d37
MattHag <e01526023@student.tuwien.ac.at>
2022-12-30 22:22:46 +0100

    Implement basic crawler
    Closes #1

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,6 +1,41 @@
 import pytest
 
+from website_checker.crawl.cookie import Cookie
+from website_checker.crawl.page import Page
+from website_checker.crawl.resource import Resource
+
 
 @pytest.fixture
-def mock_function():
-    pass
+def mock_cookies():
+    return [
+        Cookie(name="_ga"),
+        Cookie(name="_gid"),
+    ]
+
+
+@pytest.fixture
+def mock_resources():
+    return [
+        Resource(url="https://domain.test/wp-content/themes/notheme/assets/css/style.css?ver=1.0.0"),
+        Resource(url="https://www.googletagmanager.com/gtag/js?id=UA-1234567-4&l=dataLayer&cx=c"),
+    ]
+
+
+@pytest.fixture
+def mock_source():
+    return (
+        '<!DOCTYPE html><html lang="en">'
+        '<head><meta charset="UTF-8"><title>Title</title></head>'
+        '<body><h1>Heading 1</h1></body></html>'
+    )
+
+
+@pytest.fixture
+def page(mock_cookies, mock_resources, mock_source):
+    return Page(
+        url="https://domain.test",
+        title="Test page",
+        html=mock_source,
+        cookies=mock_cookies,
+        elements=mock_resources,
+    )
---
+++ website_checker/crawl/page.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from website_checker.crawl.cookie import Cookie
+
+
+class Page:
+    def __init__(self, url="", title="", html="", cookies=None, elements=None):
+        if elements is None:
+            elements = []
+        if cookies is None:
+            cookies = []
+        self.url = url
+        self.title = title
+        self.html = html
+        self.cookies = cookies
+        self.elements = elements
+        self.created = datetime.now()
+
+    def add_cookie(self, cookie: Cookie):
+        self.cookies.append(cookie)
+
+    def add_element(self, element):
+        self.elements.append(element)
