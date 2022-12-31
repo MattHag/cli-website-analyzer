27e5326427e20ac2a84d78b21906ba742647ed11
MattHag <e01526023@student.tuwien.ac.at>
2022-12-31 15:28:20 +0100

    Enables website checker run via CLI
    Relates #5, #6

--- website_checker/cli.py
+++ website_checker/cli.py
@@ -1,11 +1,14 @@
 import click
 
+from website_checker.main import WebsiteChecker
+
 
 @click.command()
 @click.argument("url")
-def cli(url):
+def main(url):
     click.echo("Given URL is: '%s'" % url)
+    WebsiteChecker().check(url)
 
 
 if __name__ == "__main__":
-    cli()
+    main()
