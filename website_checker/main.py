005a5e42893944757faaa51098d0c64ac7cccbe9
MattHag <e01526023@student.tuwien.ac.at>
2023-02-04 15:41:52 +0100

    Improve code quality in report package
    Fixes #29

--- website_checker/main.py
+++ website_checker/main.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 from website_checker.analyze.analyzer import Analyzer
 from website_checker.crawl.crawler import Crawler
-from website_checker.report.report import Report
+from website_checker.report import report
+
+PDF_OUTPUT = Path(__file__).parent.parent / "output" / "report.pdf"
 
 
 class WebsiteChecker:
     def __init__(self):
         self.analyzer = Analyzer()
-        self.reporting = Report()
 
     def check(self, url) -> Path:
         eval_list = []
@@ -17,4 +18,4 @@ class WebsiteChecker:
                 eval_result = self.analyzer.run(page)
                 eval_list.append(eval_result)
         eval_list.sort(key=lambda x: x.url)
-        return self.reporting.create(eval_list)
+        return report.PDFReport().render(eval_list, PDF_OUTPUT)
--- website_checker/report/report.py
+++ website_checker/report/report.py
@@ -1,35 +1,18 @@
-import re
+import tempfile
 from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse
 
-from jinja2 import Environment, FileSystemLoader
-from playwright.sync_api import sync_playwright
+from website_checker.report import utilities
+
+DEFAULT_TEMPLATE = Path(__file__).parent / "templates" / "base_template.html"
+DEFAULT_PDF_OUTPUT = Path(__file__).parent.parent.parent / "output" / "report.pdf"
+DEFAULT_HTML_OUTPUT = Path(__file__).parent.parent.parent / "output" / "report.html"
 
 
-def get_valid_filename(s):
-    s = str(s).strip().replace(' ', '_').replace('.', '-')
-    return re.sub(r'(?u)[^-\w.]', '', s)
-
-
-root = Path(__file__).parent.parent.parent
-
-template_dir = Path(__file__).parent / "templates"
-environment = Environment(loader=FileSystemLoader(template_dir))
-
-
-class Report:
-    def __init__(self, report_filename="report"):
-        self.report_filename = report_filename
-        self.output_dir = Path(root / "output")
-
-    def create(self, evaluation) -> Path:
-        context = self.create_context(evaluation)
-        html_file = self.render_html(context)
-        return self.render_pdf(html_file)
-
-    @staticmethod
-    def create_context(page_eval) -> dict:
+class PageToContextAdapter:
+    def __call__(self, page_eval) -> dict:
+        """Creates a context for the report."""
         if type(page_eval) is not list:
             page_eval = [page_eval]
 
@@ -39,23 +22,28 @@ class Report:
             "pages": page_eval,
         }
 
-    def render_html(self, context, template=None) -> Path:
-        if not template:
-            template = "base_template.html"
-        results_template = environment.get_template(template)
 
-        output_file = self.output_dir / f"{self.report_filename}.html"
-        with open(output_file, mode="w", encoding="utf-8") as results:
-            results.write(results_template.render(context))
-            print(f"... wrote {output_file}")
-        return output_file
+class ReportTemplate:
+    def __init__(self, html_template=DEFAULT_TEMPLATE):
+        self.html_template = html_template
 
-    def render_pdf(self, html_file) -> Path:
-        pdf_filename = self.output_dir / f"{get_valid_filename(self.report_filename)}.pdf"
-        with sync_playwright() as pw:
-            browser = pw.chromium.launch(headless=True)
-            page = browser.new_page()
-            page.goto(f"file://{html_file}")
-            page.pdf(path=pdf_filename)
-            browser.close()
-        return pdf_filename
+    def render(self, data, path: Path):
+        raise NotImplementedError
+
+
+class HTMLReport(ReportTemplate):
+    def render(self, data, path: Path) -> Path:
+        """Builds an HTML report from the given data."""
+        adapter = PageToContextAdapter()
+        context = adapter(data)
+        utilities.build_html(self.html_template, context, path)
+        return path
+
+
+class PDFReport(HTMLReport):
+    def render(self, data, path: Path) -> Path:
+        """Builds a PDF report from the given data."""
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            html = super().render(data, Path(tmp_file.name))
+            utilities.html_to_pdf(html, path)
+        return path
