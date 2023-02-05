9cee224c40771655aecc4075114f1528d7a34c0f
MattHag <e01526023@student.tuwien.ac.at>
2023-02-05 15:36:08 +0100

    Reuse logging configuration for tests
    Relates #28

--- website_checker/__init__.py
+++ website_checker/__init__.py
@@ -4,15 +4,17 @@ from loguru import logger
 
 LOG_DIR = Path(__file__).parent.parent / "logs"
 
+default_log = dict(
+    sink=LOG_DIR / "root.log",
+    rotation="1 week",
+    retention="1 month",
+    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+)
+
 
 def setup_logging():
     """Configure logging to file and console."""
-    logger.add(
-        LOG_DIR / "root.log",
-        rotation="1 week",
-        retention="1 month",
-        format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-    )
+    logger.add(**default_log)
     return logger
 
 
