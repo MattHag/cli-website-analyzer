efcbd2f7dca66e1979a6d72d93a475385bdc48e8
MattHag <e01526023@student.tuwien.ac.at>
2023-02-19 11:54:47 +0100

    CLI option: Crawl up to n pages
    Adds option:
    --max-pages n
    
    Closes #50

--- website_checker/cli.py
+++ website_checker/cli.py
@@ -1,13 +1,17 @@
 import click
+from loguru import logger
 
 from website_checker.main import WebsiteChecker
 
 
 @click.command()
 @click.argument("url")
-def main(url):
+@click.option("-p", "--max-pages", type=int, help="Crawl a maximum of n pages")
+def main(url, max_pages):
     click.echo("Given URL is: '%s'" % url)
-    WebsiteChecker().check(url)
+    if max_pages:
+        logger.debug("Crawl up to %d pages" % max_pages)
+    WebsiteChecker(max_pages=max_pages).check(url)
 
 
 if __name__ == "__main__":
