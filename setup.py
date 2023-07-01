d8eb3001a697b8f5fd68dafe3f7ad07e3a653583
MattHag <matthias.hagmann@gmx.at>
2023-07-01 16:53:27 +0200

    Move requirements to their own file
    Relates #94

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
+import io
+import os
+
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
-REQUIREMENTS = [
-    'beautifulsoup4 == 4.*',
-    'types-beautifulsoup4 == 4.*',
-    'click == 8.*',
-    'jinja2 == 3.*',
-    'loguru == 0.6.*',
-    'pandas == 1.*',
-    'pandas-stubs == 1.*',
-    'playwright == 1.*',
-    'setuptools == 58.*',
-]
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
 
-DEV_REQUIREMENTS = [
-    'black == 22.*',
-    'build == 0.7.*',
-    'flake8 == 4.*',
-    'isort == 5.*',
-    'mypy == 1.*',
-    'pre-commit == 2.20.*',
-    'pytest == 7.*',
-    'pytest-cov == 4.*',
-    'sphinx == 4.*',
-    'sphinx-rtd-theme == 1.*',
-    'twine == 4.*',
-]
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
 
 setuptools.setup(
     name='cli-website-analyzer',
@@ -53,10 +53,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=REQUIREMENTS,
-    extras_require={
-        'dev': DEV_REQUIREMENTS,
-    },
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={"dev": read_requirements("requirements-dev.txt")},
     entry_points={
         'console_scripts': [
             'website-analyzer=website_checker.cli:main',
