fb0b66888e99dab0c1b81b3b3ff7f78812c49ca7
MattHag <matthias.hagmann@gmx.at>
2023-08-01 15:04:50 +0200

    Extend integration test to two pages
    HTML files from integration_test/data are served via webserver.
    contact.html is reachable via /contact etc.
    
    Relates #60

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,3 +1,6 @@
+import os
+import threading
+from http import server
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
@@ -7,6 +10,7 @@ from loguru import logger
 from website_checker import default_log
 
 LOG_DIR = Path(__file__).parent.parent.parent / "logs"
+SERVER_DIR = Path(__file__).parent / "data"
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -25,3 +29,35 @@ def tmp_file(tmp_path):
     file = NamedTemporaryFile(delete=False)
     yield Path(file.name)
     file.close()
+
+
+class MyRequestHandler(server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Map paths to corresponding files
+        if self.path == "/":
+            self.path = "index.html"
+        elif "." not in self.path:
+            self.path = self.path + ".html"
+
+        # Call the parent class's do_GET to serve the requested file
+        return super().do_GET()
+
+
+@pytest.fixture
+def test_server():
+    server_address = ("localhost", 8000)
+    logger.info("Start server")
+    httpd = server.HTTPServer(server_address, MyRequestHandler)
+    server_url = f"http://{httpd.server_address[0]}:{httpd.server_address[1]}"
+
+    os.chdir(SERVER_DIR)  # change working directory to server directory
+
+    thread = threading.Thread(target=httpd.serve_forever)
+    thread.daemon = True
+    thread.start()
+    logger.info(f"Server running: {server_url}")
+
+    yield server_url
+
+    httpd.shutdown()
+    logger.info("Server stopped")
